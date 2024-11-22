cf9739ced08088967d171210d641e2b562134c0f
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-11-22 09:24:01 +0530

    notification service test 2

--- notification-service/src/index.ts
+++ notification-service/src/index.ts
@@ -1,14 +1,14 @@
-// import { onMessagePublished } from 'firebase-functions/v2/pubsub';
-import { onRequest } from 'firebase-functions/v2/https';
+import { onMessagePublished } from 'firebase-functions/v2/pubsub';
+// import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
 
-async function sendPushNotification(expoPushToken: string) {
+async function sendPushNotification(body: string, expoPushToken: string) {
   const message = {
     to: expoPushToken,
     sound: 'default',
-    title: 'Original Title',
-    body: 'And here is the body!',
-    data: { someData: 'goes here' },
+    title: 'Bin Notify',
+    body,
+    data: {},
   };
 
   await fetch('https://exp.host/--/api/v2/push/send', {
@@ -22,29 +22,45 @@ async function sendPushNotification(expoPushToken: string) {
   });
 }
 
-export const sendNotification = onRequest(async (_req, res) => {
-  try {
-    logger.log('Sending Message');
-    await sendPushNotification('ExponentPushToken[2wc2Z7Mu0l9qF7NocIBkTt]');
-    logger.log('Message Sent');
-
-    res.status(200).json({
-      status: 'success',
-      message: 'Notification Sent Successfully',
-    });
-  } catch (err) {
-    logger.error('Message Sending Failed');
-    logger.error(`Error: ${err}`);
-    res.status(500).json({
-      status: 'failed',
-      message: 'Notification Sending Failed',
-    });
-  }
-});
-
-// const _deviceMessageHandler = onMessagePublished(
-//   'test-topic',
-//   (_event) => {
-//     logger.debug('Log happened!');
+// export const sendNotification = onRequest(async (_req, res) => {
+//   try {
+//     logger.log('Sending Message');
+//     await sendPushNotification('ExponentPushToken[2wc2Z7Mu0l9qF7NocIBkTt]');
+//     logger.log('Message Sent');
+//
+//     res.status(200).json({
+//       status: 'success',
+//       message: 'Notification Sent Successfully',
+//     });
+//   } catch (err) {
+//     logger.error('Message Sending Failed');
+//     logger.error(`Error: ${err}`);
+//     res.status(500).json({
+//       status: 'failed',
+//       message: 'Notification Sending Failed',
+//     });
 //   }
-// );
+// });
+
+export const deviceMessageHandler = onMessagePublished(
+  'test-topic',
+  async (_event) => {
+    try {
+      // const name = event.data.message.json.name;
+      logger.log('Sending Message');
+      await sendPushNotification(
+        // `Device: ${name}`,
+        'test 2 see u',
+        'ExponentPushToken[2wc2Z7Mu0l9qF7NocIBkTt]'
+      );
+      logger.log('Message Sent');
+    } catch (err) {
+      if (err instanceof ReferenceError) {
+        logger.error('PubSub message was not JSON', err);
+      } else {
+        logger.error('Message Sending Failed');
+        logger.error(`Error: ${err}`);
+      }
+    }
+  }
+);
